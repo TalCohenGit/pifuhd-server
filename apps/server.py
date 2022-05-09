87d4a2f941ac09300803b0b938bc6464adc05edf
talCOhenGit <tulis.levi@gmail.com>
2022-05-09 14:55:37 +0300

    adding readme and last changes

--- apps/server.py
+++ apps/server.py
@@ -6,8 +6,8 @@ from flask_restful import Resource, request, Api, reqparse
 app = Flask(__name__)
 api = Api(app)
 
-PATH_TEST_IMAGE = "C:/Users/Shachar/Desktop/Projects/hdProject/pifuhd/sample_images"
-PATH_OBJ_FILE = "C:/Users/Shachar/Desktop/Projects/hdProject/pifuhd/results/pifuhd_final/recon/result_test_512.obj"
+# PATH_TEST_IMAGE = "C:/Users/Shachar/Desktop/Projects/hdProject/pifuhd/sample_images"
+# PATH_OBJ_FILE = "C:/Users/Shachar/Desktop/Projects/hdProject/pifuhd/results/pifuhd_final/recon/result_test_512.obj"
 ALLOWED_EXTENSIONS = set(['png', 'jpg'])
 
 def allowed_file(filename):
@@ -21,11 +21,11 @@ def create_obj():
         return resp
     file = request.files['file']
     if file and allowed_file(file.filename):
-        file.save(os.path.join(PATH_TEST_IMAGE, "test.png"))
+        file.save(os.path.join(request.form.get('local_path') + "/pifuhd/sample_images", "test.png"))
         resp = jsonify({'message': 'File successfully uploaded'})
         resp.status_code = 201
         building_obj_file()
-        return send_file(PATH_OBJ_FILE, mimetype='application/object')
+        return send_file(request.form.get('local_path') + "/pifuhd/results/pifuhd_final/recon/result_test_512.obj", mimetype='application/object')
     else:
         resp = jsonify({'message': 'Allowed file types are png, jpg'})
         resp.status_code = 400
